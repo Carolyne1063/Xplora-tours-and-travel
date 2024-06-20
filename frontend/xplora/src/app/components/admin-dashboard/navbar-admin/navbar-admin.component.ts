bd6549727be349b015e6f71f53912294ea5f9f5d
Carolyne1063 <carolmartoz763@gmail.com>
2024-06-19 18:36:13 -0700

    added some styles in user and admin dashboard

--- frontend/xplora/src/app/components/admin-dashboard/navbar-admin/navbar-admin.component.ts
+++ frontend/xplora/src/app/components/admin-dashboard/navbar-admin/navbar-admin.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './navbar-admin.component.css'
 })
 export class NavbarAdminComponent {
-
+  title='Xplora'
 }
--- frontend/xplora/src/app/components/user-dashboard/navbar-user/navbar-user.component.ts
+++ frontend/xplora/src/app/components/user-dashboard/navbar-user/navbar-user.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './navbar-user.component.css'
 })
 export class NavbarUserComponent {
-
+  title:string = 'Xplora'
+  navBarHome:string = 'Home'
+  navBarTours:string = 'Tours'
+  navBarAbout:string = 'About Us'
+  navBarContact:string = 'Contact Us'
 }
