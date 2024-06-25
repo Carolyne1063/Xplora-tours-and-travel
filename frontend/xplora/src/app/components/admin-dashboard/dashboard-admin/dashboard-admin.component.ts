1420a813c1585091420ec1cfd200110767b66bb4
Carolyne1063 <carolmartoz763@gmail.com>
2024-06-24 19:59:17 -0700

    edited manage user table and display all tours in the admindashboard

--- frontend/xplora/src/app/components/admin-dashboard/dashboard-admin/dashboard-admin.component.ts
+++ frontend/xplora/src/app/components/admin-dashboard/dashboard-admin/dashboard-admin.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TourService } from '../../../services/tourService/tour-service.service'; // Adjust the import path according to your project structure
+import { Tour } from '../../../interfaces/tours';// Adjust the import path according to your project structure
 
 @Component({
   selector: 'app-dashboard-admin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard-admin.component.html',
-  styleUrl: './dashboard-admin.component.css'
+  styleUrls: ['./dashboard-admin.component.css']
 })
-export class DashboardAdminComponent {
+export class DashboardAdminComponent implements OnInit {
+  tours: Tour[] = [];
 
+  constructor(private tourService: TourService) {}
+
+  ngOnInit(): void {
+    this.fetchAllTours();
+  }
+
+  fetchAllTours(): void {
+    this.tourService.getAllTours().subscribe(
+      (tours) => {
+        this.tours = tours;
+      },
+      (error) => {
+        console.error('Error fetching tours:', error);
+      }
+    );
+  }
 }
